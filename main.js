c74214406bbdde699cfcf1c395f1e1d600850445
Samira Esquina <samiraesquina@gmail.com>
2019-08-23 17:23:02 -0400

    edited html css

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-$(document).ready(function () {
-    $('.menu-toggler').on('click', function () {
-        $(this).toggleClass('open');
-        $('.top-nav').toggleClass('open');
-    });
+$(document).ready(function() {
+  $('.menu-toggler').on('click', function() {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  });
 
-    $('.top-nav .nav-link').on('click', function () {
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
-    });
+  $('.top-nav .nav-link').on('click', function() {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
 
-    $('nav a[href*="#"]').on('click', function () {
-        $('html, body').animate({
-            scrollTop:$($(this).attr('href')).offset().top - 100
-        }, 2000);
-    });
+  $('nav a[href*="#"]').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 2000);
+  });
 
-    $('#up').on('click', function () {
-        $('html, body').animate({
-            scrollTop:0
-        }, 2000);
-    });
+  $('#up').on('click', function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 2000);
+  });
 
-    AOS.init({
-        easing: 'ease',
-        duration: 1800,
-        once:true
-    });
+  AOS.init({
+    easing: 'ease',
+    duration: 1800,
+    once: true
+  });
 });
 
 
 // jQuery Slider //
 $(document).ready(function() {
-  $('.prev').on('click', function(){
+  $('.prev').on('click', function() {
     var prevImg = $('img.active').prev('.slider-inner img');
-    if(prevImg.length == 0) {
+    if (prevImg.length == 0) {
       prevImg = $('.slider-inner img:last');
     }
     $('img.active').removeClass('active');
@@ -41,7 +41,7 @@ $(document).ready(function() {
   });
   $('.next').on('click', function() {
     var nextImg = $('img.active').next('.slider-inner img');
-    if(nextImg.length == 0) {
+    if (nextImg.length == 0) {
       nextImg = $('.slider-inner img:first');
     }
     $('img.active').removeClass('active');
