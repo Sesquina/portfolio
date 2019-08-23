6f8f6cff9ca9e957a1012c24a6aa599ffb5aff73
Samira Esquina <samiraesquina@gmail.com>
2019-08-22 21:12:54 -0400

    Edited HTML/CSS

--- main.js
+++ main.js
@@ -1,54 +1,3 @@
-
-const items = document.querySelectorAll('img');
-const itemCount = items.length;
-const nextItem = document.querySelector('.next');
-const previousItem = document.querySelector('.previous');
-let count = 0;
-
-function showNextItem() {
-  items[count].classList.remove('active');
-
-  if(count < itemCount - 1) {
-    count++;
-  } else {
-    count = 0;
-  }
-
-  items[count].classList.add('active');
-  console.log(count);
-}
-
-function showPreviousItem() {
-  items[count].classList.remove('active');
-
-  if(count > 0) {
-    count--;
-  } else {
-    count = itemCount - 1;
-  }
-
-  items[count].classList.add('active');
-  console.log(count);
-}
-
-function keyPress(e) {
-  e = e || window.event;
-
-  if (e.keyCode == '37') {
-    showPreviousItem();
-  } else if (e.keyCode == '39') {
-    showNextItem();
-  }
-}
-
-nextItem.addEventListener('click', showNextItem);
-previousItem.addEventListener('click', showPreviousItem);
-document.addEventListener('keydown', keyPress);
-
-
-
-
-
 $(document).ready(function () {
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
@@ -78,4 +27,24 @@ $(document).ready(function () {
         once:true
     });
 });
-//
+
+
+// jQuery Slider //
+$(document).ready(function() {
+  $('.prev').on('click', function(){
+    var prevImg = $('img.active').prev('.slider-inner img');
+    if(prevImg.length == 0) {
+      prevImg = $('.slider-inner img:last');
+    }
+    $('img.active').removeClass('active');
+    prevImg.addClass('active');
+  });
+  $('.next').on('click', function() {
+    var nextImg = $('img.active').next('.slider-inner img');
+    if(nextImg.length == 0) {
+      nextImg = $('.slider-inner img:first');
+    }
+    $('img.active').removeClass('active');
+    nextImg.addClass('active');
+  });
+});
